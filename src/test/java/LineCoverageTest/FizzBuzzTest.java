2119507db6b740410ba1d41175529c690b47fb5d
Hamza <hamzaots31@gmail.com>
2024-03-22 17:02:49 +0100

    commit

--- src/test/java/LineCoverageTest/FizzBuzzTest.java
+++ src/test/java/LineCoverageTest/FizzBuzzTest.java
@@ -1,4 +1,24 @@
 package LineCoverageTest;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.src.FizzBuzz;
+
 public class FizzBuzzTest {
+    @Test
+    public void ShouldReturnExeptionIfIsInfThenone(){
+        Assertions.assertThrows(IllegalArgumentException.class , ()-> FizzBuzz.fizzBuzz(0));
+    }
+    @Test
+    public void ShouldReturnFizzIfDivisibleThree(){
+        Assertions.assertEquals("Fizz",FizzBuzz.fizzBuzz(9));
+    }
+    @Test
+    public void ShouldReturnBuzzIfDivisibleFive(){
+        Assertions.assertEquals("Buzz",FizzBuzz.fizzBuzz(10));
+    }
+    @Test
+    public void ShouldReturnFizzBuzzIfDivisibleThreeAndFive(){
+        Assertions.assertEquals("FizzBuzz",FizzBuzz.fizzBuzz(30));
+    }
 }
